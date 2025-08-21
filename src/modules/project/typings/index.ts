dce39bbd4d005645d2fa6f66dae0e4919b4b4c60
Renan Prado <renanprado96@gmail.com>
2025-08-21 16:58:48 -0300

    feat: adicionar configuração de indicadores de desenvolvimento e atualizar lógica de acesso para tenant 'reglo'

--- src/modules/project/typings/index.ts
+++ src/modules/project/typings/index.ts
@@ -2,5 +2,4 @@ export type ProjectMinimal = {
   slug: string
   name: string
   avatar: string
-  plan: 'Free' | 'Starter' | 'Enterprise'
 }
--- src/modules/tenant/components/team-switch-ui.tsx
+++ src/modules/tenant/components/team-switch-ui.tsx
@@ -41,22 +41,21 @@ export function TeamSwitchUi({ teams }: { teams: ProjectMinimal[] }) {
           <DropdownMenuTrigger asChild>
             <SidebarMenuButton
               size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground h-12"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground h-12 gap-1"
             >
-              <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+              <div className="text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
                 <Image
                   src={activeTeam.avatar}
                   alt={activeTeam.name}
-                  width={32}
-                  height={32}
-                  className="rounded-lg"
+                  width={24}
+                  height={24}
+                  className="rounded-md"
                 />
               </div>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">{activeTeam.name}</span>
-                <span className="truncate text-xs">{activeTeam.plan}</span>
               </div>
-              <ChevronsUpDown className="ml-auto" />
+              <ChevronsUpDown className="mr-1 ml-auto" />
             </SidebarMenuButton>
           </DropdownMenuTrigger>
           <DropdownMenuContent
