4a9cc8ea3a69f6aa39b3fba2fdbdcfa7b3e9322e
Renan Prado <renanprado96@gmail.com>
2025-09-13 15:16:59 -0300

    feat: adicionar estilo de seleção e refatorar componente RulePage para melhorar a usabilidade

---
+++ src/modules/rules/components/context-menu-rule/context-menu-rule.tsx
@@ -0,0 +1,7 @@
+export function ContextMenuRule() {
+  return (
+    <div>
+      <h1>ContextMenuRule</h1>
+    </div>
+  )
+}
--- src/modules/rules/pages/RulePage.tsx
+++ src/modules/rules/pages/RulePage.tsx
@@ -6,6 +6,7 @@ import { Separator } from '@/components/ui/separator'
 import {
   Box,
   Boxes,
+  ChevronRight,
   Dot,
   FolderPlus,
   MessageCircle,
@@ -17,60 +18,58 @@ type RuleProps = {
   title: string
   description: string
   id: string
-  isLast: boolean
   comments: {
     quantity: number
   }
 }
 
-function Rule({ title, description, id, isLast, comments }: RuleProps) {
+function Rule({ title, description, id, comments }: RuleProps) {
   return (
-    <div data-component="rule" className="flex w-full items-stretch gap-4">
-      <div className="flex w-6 flex-col items-center">
-        <div className="bg-ndl-primary/60 mt-[3px] flex shrink-0 items-center justify-center rounded-full">
-          <Dot className="size-5 text-white" />
-        </div>
-        <span className="h-full w-[3px] bg-zinc-200" />
-        {isLast && (
-          <span className="size-3 min-h-3 min-w-3 rounded-full bg-zinc-200" />
-        )}
-      </div>
-      <div
-        data-is-last={isLast}
-        className="flex w-full flex-col gap-2 data-[is-last=false]:pb-8"
-      >
-        <div className="flex w-full flex-col gap-4 rounded-md border-2 border-l-4 border-zinc-200 border-l-blue-700/60 px-4 py-3 shadow-xs">
-          <div className="flex w-full gap-3">
-            <span className="text-xss flex items-center justify-center rounded-md border px-2 text-zinc-400">
-              #{id}
-            </span>
-            <div className="flex items-center gap-2">
-              <Box className="size-3.5 text-zinc-400" />
-              <h3 className="text-base font-bold text-zinc-700">{title}</h3>
-            </div>
-          </div>
-          <span className="flex w-full flex-1 py-2 pl-1 text-base font-normal text-gray-700">
-            {description}
+    <div
+      data-component="rule"
+      className="flex w-full cursor-pointer items-stretch gap-4 rounded-md hover:bg-zinc-50"
+    >
+      <div className="flex w-full justify-between gap-2 px-4 py-6">
+        <div className="flex gap-3">
+          <span className="text-xss mt-0.5 flex h-6 w-fit items-center justify-center rounded-md border px-2 text-zinc-400">
+            {id}
           </span>
-          <div className="flex items-center gap-1 border-t border-zinc-200 pt-2 text-zinc-400">
-            <Button
-              variant="ghost"
-              size="sm"
-              className="flex items-center gap-1"
-            >
-              <MessageCircle className="relative -top-[0.5px] size-4" />
-              <span className="font-semibold">{comments.quantity}</span>
+          <div className="flex flex-col gap-2">
+            <h3 className="text-lg font-semibold text-zinc-700">{title}</h3>
+            <span className="flex-1text-base flex w-full font-normal text-zinc-500">
+              {description}
+            </span>
+          </div>
+        </div>
+        <Button variant="ghost" size="sm">
+          <MoreHorizontal />
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+function FolderRule({ title, id }: { title: string; id: string }) {
+  return (
+    <div
+      data-component="folder-rule"
+      className="flex w-full cursor-pointer items-stretch gap-4 rounded-md hover:bg-zinc-50"
+    >
+      <div className="flex w-full justify-between gap-2 px-4 py-6">
+        <div className="flex w-full items-center gap-2">
+          <span className="text-xss flex h-6 w-fit items-center justify-center rounded-md border px-2 text-zinc-400">
+            {id}
+          </span>
+          <div className="flex flex-col gap-2">
+            <Button variant="outline" size="sm">
+              <Boxes strokeWidth={1} />
+              <span>{title}</span>
             </Button>
           </div>
         </div>
-
-        {isLast && (
-          <div className="flex gap-2 pt-4">
-            <Button variant="outline" size="sm">
-              <PackagePlus />
-            </Button>
-          </div>
-        )}
+        <Button variant="ghost" size="sm">
+          <MoreHorizontal />
+        </Button>
       </div>
     </div>
   )
@@ -86,7 +85,7 @@ export function RulePage() {
         <div className="flex w-full flex-1 flex-col gap-1 pl-4">
           <div className="flex w-full items-center justify-between">
             <div className="flex items-center gap-3">
-              <h1 className="text-3xl font-bold">Página de Produto</h1>
+              <h1 className="text-3xl font-semibold">Página de Produto</h1>
               <span className="text-xss mt-1 flex h-6 w-fit items-center justify-center rounded-md border px-2 text-zinc-400">
                 #e62n48f
               </span>
@@ -94,14 +93,6 @@ export function RulePage() {
             <Button variant="ghost" size="sm">
               <MoreHorizontal />
             </Button>
-          </div>
-          <div className="mt-2 flex max-w-xl">
-            <span className="text-sm text-zinc-500">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Laudantium, quas voluptate laboriosam neque ipsum quisquam quis
-              placeat officia iste corrupti! Explicabo qui autem quae deserunt
-              voluptatem ducimus ipsum amet nobis!
-            </span>
           </div>
 
           <div className="flex w-full items-center justify-between gap-3 pt-2">
@@ -158,43 +149,25 @@ export function RulePage() {
           </div>
         </div>
       </section>
-      <div className="bg-muted flex flex-1 flex-col gap-4 p-4">
-        <Card>
-          <CardDescription className="flex w-full flex-col gap-6 px-6">
-            <div className="flex gap-3 pl-10">
-              <Button variant="outline" size="sm">
-                <Boxes strokeWidth={1} />
-                <span>Descrição do produto</span>
-              </Button>
-              <Button variant="outline" size="sm">
-                <Boxes strokeWidth={1} />
-                <span>Compre junto</span>
-              </Button>
-              <div className="flex gap-2">
-                <Button variant="outline" size="sm">
-                  <FolderPlus />
-                </Button>
-              </div>
-            </div>
 
-            <div className="flex w-full flex-col">
-              <Rule
-                title="Usuário logado"
-                description="Usuário logado"
-                id="e62n48f"
-                isLast={false}
-                comments={{ quantity: 1 }}
-              />
-              <Rule
-                title="Usuário logado"
-                description="Usuário logado"
-                id="e62n48f"
-                isLast={true}
-                comments={{ quantity: 1 }}
-              />
-            </div>
-          </CardDescription>
-        </Card>
+      <div className="flex flex-1 flex-col gap-4 bg-white p-4">
+        <div className="flex w-full flex-col">
+          <Rule
+            title="Login obrigatório para dashboard"
+            description="O usuário deve estar logado para acessar o dashboard principal do sistema."
+            id="#e62n48f"
+            comments={{ quantity: 1 }}
+          />
+
+          <FolderRule title="Autenticação" id="#e62n48f" />
+
+          <Rule
+            title="Timeout de sessão"
+            description="A sessão do usuário expira após 30 minutos de inatividade."
+            id="#e62n48f"
+            comments={{ quantity: 1 }}
+          />
+        </div>
       </div>
     </div>
   )
